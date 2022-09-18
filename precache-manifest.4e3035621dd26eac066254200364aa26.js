059e642a63931d1290e107993f1c4a84396407e3
eber-cba <ebercoronel29@gmail.com>
2022-09-18 00:38:51 -0300

    Updates

---
+++ precache-manifest.4e3035621dd26eac066254200364aa26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5eba091a207d9231a0b",
+    "url": "/pokemonChallenge/static/css/main.f6b43e4c.chunk.css"
+  },
+  {
+    "revision": "e5eba091a207d9231a0b",
+    "url": "/pokemonChallenge/static/js/main.e5eba091.chunk.js"
+  },
+  {
+    "revision": "e64d281d7a69deae9725",
+    "url": "/pokemonChallenge/static/js/1.e64d281d.chunk.js"
+  },
+  {
+    "revision": "ded538c2cff46b2545b5",
+    "url": "/pokemonChallenge/static/js/runtime~main.ded538c2.js"
+  },
+  {
+    "revision": "9aa86886b986e68c2a4196db8b3ea9f8",
+    "url": "/pokemonChallenge/static/media/769px-Pokebola-pokeball-png-0.9aa86886.png"
+  },
+  {
+    "revision": "148d54341d6a23c49f66fd82babe84b4",
+    "url": "/pokemonChallenge/index.html"
+  }
+];
